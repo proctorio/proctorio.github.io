d8a551aba6b2b01d0efdc56e757c5872e10cd7ed
Mike Olsen <artfulhacker@echovoice.com>
2018-10-20 22:38:59 -0700

    Create canvas_global.js

---
+++ snippets/canvas_global.js
@@ -0,0 +1,1 @@
+(function(){var a=document.createElement("script"),b=document.head||document.getElementsByTagName("head")[0];a.onload=function(){b.removeChild(a)};a.src="//cdn.proctorio.net/snippets/gbl/canvas-global-embed.js";b.appendChild(a)})();
