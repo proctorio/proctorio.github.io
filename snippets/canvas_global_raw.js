feb0ff71587ee10b8dd6ef5af140da3debe35b5c
Mike Olsen <artfulhacker@echovoice.com>
2018-11-04 20:39:21 -0700

    add raw, non-minified version

---
+++ snippets/canvas_global_raw.js
@@ -0,0 +1,2 @@
+/* THIS VERSION IS NOT RECOMMENDED, SEE GITHUB.COM/PROCTORIO/PROCTORIO.GITHUB.IO */
+(function(){var a=document.createElement("script"),b=document.head||document.getElementsByTagName("head")[0];a.onload=function(){b.removeChild(a)};a.src="//raw.githubusercontent.com/proctorio/proctorio.github.io/master/snippets/gbl/canvas-global-embed.js";b.appendChild(a)})();
