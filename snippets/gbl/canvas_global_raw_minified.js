fa0470e13ee2355482ca879b9095f4ae323060e9
Mike Olsen <artfulhacker@echovoice.com>
2018-11-04 20:29:12 -0700

    add github raw served version

---
+++ snippets/gbl/canvas_global_raw_minified.js
@@ -0,0 +1,1 @@
+(function(){var a=document.createElement("script"),b=document.head||document.getElementsByTagName("head")[0];a.onload=function(){b.removeChild(a)};a.src="//raw.githubusercontent.com/proctorio/proctorio.github.io/master/snippets/gbl/canvas-global-embed.min.js";b.appendChild(a)})();
