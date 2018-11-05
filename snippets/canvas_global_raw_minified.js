fb2607a1c0d1e345e190a9e7c0bf516249ce6d09
Mike Olsen <artfulhacker@echovoice.com>
2018-11-04 20:36:50 -0700

    add not recommended comment to file

--- snippets/canvas_global_raw_minified.js
+++ snippets/canvas_global_raw_minified.js
@@ -1,1 +1,2 @@
+/* THIS VERSION IS NOT RECOMMENDED, SEE GITHUB.COM/PROCTORIO/PROCTORIO.GITHUB.IO */
 (function(){var a=document.createElement("script"),b=document.head||document.getElementsByTagName("head")[0];a.onload=function(){b.removeChild(a)};a.src="//raw.githubusercontent.com/proctorio/proctorio.github.io/master/snippets/gbl/canvas-global-embed.min.js";b.appendChild(a)})();
