2d2e18af47cb04b8eaefd73d2eef56793112f1d1
Mike Olsen <artfulhacker@echovoice.com>
2018-11-04 20:39:56 -0700

    add not recommended comment

--- snippets/canvas_global.js
+++ snippets/canvas_global.js
@@ -1,1 +1,2 @@
+/* THIS VERSION IS NOT RECOMMENDED, SEE GITHUB.COM/PROCTORIO/PROCTORIO.GITHUB.IO */
 (function(){var a=document.createElement("script"),b=document.head||document.getElementsByTagName("head")[0];a.onload=function(){b.removeChild(a)};a.src="//cdn.proctorio.net/snippets/gbl/canvas-global-embed.js";b.appendChild(a)})();
